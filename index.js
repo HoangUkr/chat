fa6edb6a3d623d5fa6f74e9322136d139ac2fc9f
Pham Xuan Hoang <hoangukraine@yahoo.com>
2020-01-29 21:53:09 +0100

    Continue Exercise 2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 var fs = require('fs');
 const app = express();
-const https = require('https');
-const io = require('socket.io')(https);
+const http = require('http');
+const io = require('socket.io')(http);
 var formidable = require('formidable');
 var list_user = {};
 let port = process.env.PORT || 3000;
@@ -88,14 +88,15 @@ io.sockets.on('connection', function(socket){
         socket.broadcast.emit('typing', {username: socket.username});
     });
 });
-/*
+
 const server = http.listen(port, function(){
     console.log('port: 3000, Ready!');
 });
-*/
+/*
 https.createServer({
     key: fs.readFileSync('server.key'),
     cert: fs.readFileSync('server.cert')
 },app).listen(port, function(){
     console.log('port: 3000, Ready!!!');
 });
+*/
